e6ad213360583d87bca3ca082a55b1cb83a65b13
Andrey <positko.andrei@gmail.com>
2019-07-16 21:07:13 +0300

    Commit_SignUp

---
+++ src/sample/SignUpController.java
@@ -0,0 +1,47 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private PasswordField signUpCountry;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField signUpName;
+
+    @FXML
+    private TextField signUpLastName;
+
+    @FXML
+    private TextField login_field11;
+
+    @FXML
+    private TextField login_field12;
+
+    @FXML
+    private CheckBox signUpCheckBoxMale;
+
+    @FXML
+    private CheckBox signUpCheckBoxFemale;
+
+    @FXML
+    void initialize() {
+
+    }
+}
