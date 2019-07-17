2a42d0746996caba8f539f05d5567aa2c86ff06b
Andrey <positko.andrei@gmail.com>
2019-07-17 18:02:37 +0300

    DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,18 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOException;
 
 public class Controller {
 
@@ -29,8 +36,24 @@ public class Controller {
 
     @FXML
     void initialize() {
-        authSignInButton.setOnAction(event -> {
-            System.out.println("Вы нажали на кнопку Войти");
+        loginSignUpButton.setOnAction(event -> {
+            loginSignUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
         });
     }
 }
